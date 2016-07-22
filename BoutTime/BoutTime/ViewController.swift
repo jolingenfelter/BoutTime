aab564e489a64b09266f6beac073d0729f170688
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-07-22 11:47:28 -0400

    Main ViewController setup in storyboard

--- BoutTime/BoutTime/ViewController.swift
+++ BoutTime/BoutTime/ViewController.swift
@@ -9,6 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Game Variables
+    var roundsCompleted = 0
+    var numberOfCorrectRounds = 0
+    var numberOfRoundsCompleted = 0
+    var totalNumberOfRounds = 6
+    var indexOfEvent = 0
+    var eventsList: [Event] = []
+    var currentRoundEvents: [Event] = []
+    var nextRoundEvents: [Event] = []
+    
+    //Labels
+    @IBOutlet weak var event1Label: UILabel!
+    @IBOutlet weak var event2Label: UILabel!
+    @IBOutlet weak var event3Label: UILabel!
+    @IBOutlet weak var event4Label: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var shakeToComplete: UILabel!
+    
+    
+    //Buttons
+    @IBOutlet weak var downButton1: UIButton!
+    @IBOutlet weak var upButton1: UIButton!
+    @IBOutlet weak var downButton2: UIButton!
+    @IBOutlet weak var upButton2: UIButton!
+    @IBOutlet weak var downButton3: UIButton!
+    @IBOutlet weak var upButton3: UIButton!
+    
+    required init?(coder aDecoder: NSCoder) {
+        do {
+            let array = try PlistConverter.arrayFromFile("PixarEvents", ofType: "plist")
+            self.eventsList = EventsUnarchiver.eventsFromArray(array)
+        } catch let error {
+            fatalError("\(error)")
+        }
+        super.init(coder: aDecoder)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
